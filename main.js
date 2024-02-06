282d5bfab914f34cb6543b0a441ddcda14d1aefb
meatguy420 <157437849+meatguy420@users.noreply.github.com>
2024-02-05 23:50:45 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var greating = "Hello";
+
+var name = "Class!";
+
+alert(greating + name);
+
+username = window.prompt("What is your name?")
+
+console.log(username)
+
+/*
+function changeBackground(color) {
+    document.body.style.background = color;
+}
+
+window.addEventListener.apply("load",function() { changeBackground('blue')})
+*/
+
+console.log("name");
